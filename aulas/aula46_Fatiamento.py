9f1d6e09923f968171d5a612c599052401975b62
Victor <128427578+Victorhugofcaetano@users.noreply.github.com>
2023-10-22 00:32:29 -0300

    teste

---
+++ aulas/aula46_Fatiamento.py
@@ -0,0 +1,11 @@
+'''
+Fatiamento de Strings
+012345678
+Olá Mundo
+-987654321
+Fatiamento [i:f:p] [::]
+Obs.: a função len retorna a qtd
+de caracteres de str
+'''
+variavel = 'Olá mundo'
+print(variavel)
